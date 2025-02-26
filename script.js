d5729d1a43bb429ff08cb64d5e70f353e02308be
esca0063@student.acitech.org <esca0063@student.acitech.org>
2025-02-26 09:13:30 -0500

    ygf

---
+++ script.js
@@ -0,0 +1,9 @@
+const button1 = document.getElementById("button1")
+let clicks = 0
+function yellatuser(){
+    alert("STOP TOUCHING ME!!!")
+}
+button1.addEventListener("click", yellatuser);
+if (button1, "click") {clicks++;
+    console.log(clicks)
+}
