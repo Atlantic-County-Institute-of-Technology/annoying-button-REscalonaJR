cce1b3e8150875c97b233959de41c9db4c894faf
esca0063@student.acitech.org <esca0063@student.acitech.org>
2025-03-07 09:15:45 -0500

    2nd to last commit

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let texxt = document.getElementById("alarmtext");
 let stache = document.getElementById("must")
 let container = document.getElementById("container")
 let clicks = 0;
-let clicksound = new Audio ('assets/click.mp3')
+let clicksound = new Audio ('assets/click.mp3');
+const button2 = document.createElement("button");
+ const button3 = document.createElement("button");
+ container.appendChild(button3);
+container.appendChild(button2)
+button2.classList.add("invis")
+button3.classList.add("invis")
+
 
 function yellatuser(){
     clicks++;clicksound.play();
@@ -88,24 +95,50 @@ function yellatuser(){
     if (clicks==30) {
       texxt.innerHTML = "I CANT ANYMORE"
       button1.classList.remove("running")
-      const button2 = document.createElement("button")
+      button2.classList.remove("invis")
+button3.classList.remove("invis")
       button2.classList.add("button1")
-      container.appendChild(button2)
       button1.innerHTML = "NOT ME"
       button2.innerHTML = "NOT ME"
       button1.classList.add("taco")
       button1.classList.remove("initial")
-      const button3 = document.createElement("button")
-      container.appendChild(button3)
       button3.classList.add("button1")
       button3.classList.add("yeah")
       button3.innerHTML = "ME"
-      button1.classList.add("dash")
+      
     }
-    if (clicks ==35){
-      button1.classList.remove("dash")
+    if (clicks ==32){
+
+      button2.classList.add("invis")
+button3.classList.add("invis")
+button2.classList.add("button1")
+button3.classList.remove("button1")
+      texxt.innerHTML = "WHY DO YOU INSIST ON CLICKING ME!!!"
+      button1.classList.remove("taco")
+      button1.classList.add("initial")
+      button1.classList.add("growshrink")
+      button1.innerHTML=""
+    }
+    if (clicks==33){
+      button1.classList.remove("growshrink")
+      texxt.innerHTML="i give up."
+      button1.innerHTML="DONT PRESS"
+    }
+    if (clicks==34) {
+      texxt.innerHTML="i cant do this anymore"
+    }
+    if (clicks==35) {
+      texxt.innerHTML="goodbye"
+    }
+    if(clicks== 40){
+    texxt.innerHTML = "WHY WONT YOU STOP"
+    }
+    if (clicks==41) {
+      texxt.innerHTML = "here, something until 100"
+    button1.classList.add("running")
     }
     if(clicks==100){
+      button1.classList.remove("running")
       stache.classList.remove("stache")
       button1.classList.add("mover")
       texxt.innerHTML = "I HAVE REACHED MY ULTIMATE STATE YOU CANT CATCH ME"
