1eca5bef111bd272dff858978ee149298f064c03
esca0063@student.acitech.org <esca0063@student.acitech.org>
2025-03-10 09:11:30 -0400

    final commit

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ let stache = document.getElementById("must")
 let container = document.getElementById("container")
 let clicks = 0;
 let clicksound = new Audio ('assets/click.mp3');
+let mercury = document.getElementById("mercury");
+let venus = document.getElementById("venus");
+let earth = document.getElementById("earth");
+let jupiter = document.getElementById("jupiter");
+let saturn = document.getElementById("staurn");
+let uranus = document.getElementById("uranus");
+let neptune = document.getElementById("neptune");
+let sun = document.getElementById("sun");
+let crewmate = document.getElementById("crewmate");
+let star_a = document.getElementById("star_a");
+let sta_a = document.getElementById("sta_a");
+let stare_a = document.getElementById("stare_a");
+let star_b = document.getElementById("star_b");
+let sta_b = document.getElementById("sta_b");
+let stare_b = document.getElementById("stare_b");
+let star_c = document.getElementById("star_c");
+let sta_c = document.getElementById("sta_c");
+let stare_c = document.getElementById("stare_c");
+let solar_container = document.getElementById("solar_container")
+let body = document.getElementById("body")
+let button_trail1 = document.getElementById("button_trail1")
+let button_trail2 = document.getElementById("button_trail2")
 const button2 = document.createElement("button");
  const button3 = document.createElement("button");
  container.appendChild(button3);
@@ -14,6 +36,7 @@ button3.classList.add("invis")
 
 function yellatuser(){
     clicks++;clicksound.play();
+    console.log(clicks);
     if(clicks==0){
       texxt.innerHTML=""
     }
@@ -111,13 +134,16 @@ button3.classList.remove("invis")
 
       button2.classList.add("invis")
 button3.classList.add("invis")
-button2.classList.add("button1")
+button2.classList.remove("button1")
 button3.classList.remove("button1")
       texxt.innerHTML = "WHY DO YOU INSIST ON CLICKING ME!!!"
       button1.classList.remove("taco")
       button1.classList.add("initial")
       button1.classList.add("growshrink")
-      button1.innerHTML=""
+      button1.innerHTML="CLICK"
+      button3.innerHTML=""
+      button2.innerHTML=""
+
     }
     if (clicks==33){
       button1.classList.remove("growshrink")
@@ -133,16 +159,59 @@ button3.classList.remove("button1")
     if(clicks== 40){
     texxt.innerHTML = "WHY WONT YOU STOP"
     }
-    if (clicks==41) {
-      texxt.innerHTML = "here, something until 100"
-    button1.classList.add("running")
+    if(clicks==41){
+      button1.classList.remove("initial");
+      button1.classList.add("mom_look_im_a_star");
+      texxt.innerHTML = "WIP";
+     body.classList.add("bg")
+      
+      button1.innerHTML="Don't Press";
+      star_a.classList.add("star_a")
+      sta_a.classList.add("sta_a")
+      stare_a.classList.add("stare_a")
+      star_b.classList.add("star_b")
+      sta_b.classList.add("sta_b")
+      stare_b.classList.add("stare_b")
+      star_c.classList.add("star_c")
+      sta_c.classList.add("sta_c")
+      stare_c.classList.add("stare_c")
+      star_a.classList.remove("invis")
+      sta_a.classList.remove("invis")
+      stare_a.classList.remove("invis")
+      star_b.classList.remove("invis")
+      sta_b.classList.remove("invis")
+      stare_b.classList.remove("invis")
+      star_c.classList.remove("invis")
+      sta_c.classList.remove("invis")
+      stare_c.classList.remove("invis")
     }
+   
     if(clicks==100){
-      button1.classList.remove("running")
-      stache.classList.remove("stache")
+      button1.classList.add("initial");
+      button1.classList.remove("mom_look_im_a_star");
+      body.classList.remove("bg")
+      star_a.classList.remove("star_a")
+      sta_a.classList.remove("sta_a")
+      stare_a.classList.remove("stare_a")
+      star_b.classList.remove("star_b")
+      sta_b.classList.remove("sta_b")
+      stare_b.classList.remove("stare_b")
+      star_c.classList.remove("star_c")
+      sta_c.classList.remove("sta_c")
+      stare_c.classList.remove("stare_c")
+      star_a.classList.add("invis")
+      sta_a.classList.add("invis")
+      stare_a.classList.add("invis")
+      star_b.classList.add("invis")
+      sta_b.classList.add("invis")
+      stare_b.classList.add("invis")
+      star_c.classList.add("invis")
+      sta_c.classList.add("invis")
+      stare_c.classList.add("invis")
       button1.classList.add("mover")
       texxt.innerHTML = "I HAVE REACHED MY ULTIMATE STATE YOU CANT CATCH ME"
     }
 }
+
 
 button1.addEventListener("click", yellatuser);
